1ca1e9da289ae6fb05de0b7b1eca10cd5cf20f7c
James Thomason <vandebar@gmail.com>
2012-08-31 09:29:43 -0500

    Remove old event table

--- app/models/event.rb
+++
@@ -1,7 +0,0 @@
-class Event < ActiveRecord::Base
-  attr_accessible :act_id, :gig_id
-
-	belongs_to :act
-	belongs_to :gig
-	
-end
--- db/migrate/20120829223509_add_gig_id_act_id_to_sets.rb
+++
@@ -1,6 +0,0 @@
-class AddGigIdActIdToSets < ActiveRecord::Migration
-  def change
-    add_column :sets, :gig_id, :integer
-    add_column :sets, :act_id, :integer
-  end
-end
